44c7988b47c799f4e25904bd0bd9f98d53f2273e
aurelien14 <aurelien.ah14@gmail.com>
2026-01-02 19:12:21 +0100

    Add static system configuration

--- PLCMaster/src/system/config/config_static.c
+++ PLCMaster/src/system/config/config_static.c
@@ -2,7 +2,53 @@
 
 #include "config_static.h"
 
-int config_static_load(void)
+static const BackendConfig_t kBackendConfigs[] = {
+    {
+        .name = "ec0",
+        .ifname = "TODO_IFNAME",
+        .cycle_time_us = 1000,
+        .dc_clock = true,
+    },
+};
+
+static const DeviceConfig_t kDeviceConfigs[] = {
+    {
+        .name = "CPU_IO",
+        .model = "L230",
+        .backend = "ec0",
+        .ethercat =
+            {
+                .expected_position = 1,
+            },
+        .io_addr = 1,
+        .expected_identity =
+            {
+                .vendor_id = 0,    /* TODO: set vendor id */
+                .product_code = 0, /* TODO: set product code */
+            },
+    },
+};
+
+static const ProcessVarDesc kProcessVarDescs[] = {
+    /* TODO: populate process variables */
+};
+
+static const HmiTagDesc kHmiTagDescs[] = {
+    /* TODO: populate HMI tags */
+};
+
+static const SystemConfig_t kStaticConfig = {
+    .backends = kBackendConfigs,
+    .backend_count = sizeof(kBackendConfigs) / sizeof(kBackendConfigs[0]),
+    .devices = kDeviceConfigs,
+    .device_count = sizeof(kDeviceConfigs) / sizeof(kDeviceConfigs[0]),
+    .process_vars = kProcessVarDescs,
+    .process_var_count = sizeof(kProcessVarDescs) / sizeof(kProcessVarDescs[0]),
+    .hmi_tags = kHmiTagDescs,
+    .hmi_tag_count = sizeof(kHmiTagDescs) / sizeof(kHmiTagDescs[0]),
+};
+
+const SystemConfig_t *get_static_config(void)
 {
-    return -1;
+    return &kStaticConfig;
 }
